bb8a5f8901fc538ce5a8dfd75e84e98367ee8b06
imrahulsrivastava <imrahulsrivastava1@gmail.com>
2024-04-26 12:50:49 +0530

    corrected App.js and App.css

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from "react";
 import "../styles/App.css";
 
-const App = () => {
+function App() {
   const [showBall, setShowBall] = useState(false);
   const [ballPosition, setBallPosition] = useState(0);
 
+  const RenderChoice = () =>
+    showBall ? (
+      <div className="ball" style={{ left: ballPosition + "px" }}></div>
+    ) : (
+      <button className="start" onClick={buttonClickHandler}>
+        start
+      </button>
+    );
+
+  const buttonClickHandler = () => setShowBall(!showBall);
+
   const handleKeyDown = (e) => {
-    if (e.key === "ArrowRight") {
-      setBallPosition((prev) => prev + 5);
+    if (e.key === "ArrowRight" || e.keyCode === 39) {
+      setBallPosition((position) => position + 5);
+    } else if (e.key === "ArrowLeft" || e.keyCode === 37) {
+      setBallPosition((position) => position - 5);
     }
   };
 
@@ -18,18 +31,11 @@ const App = () => {
     };
   }, []);
 
-  const renderBallOrButton = () =>
-    showBall ? (
-      <div className="ball" style={{ right: ballPosition + "px" }}>
-        ball
-      </div>
-    ) : (
-      <button onClick={() => setShowBall(true)} className="start">
-        start
-      </button>
-    );
-
-  return <div className="playground">{renderBallOrButton()}</div>;
-};
+  return (
+    <div className="playground">
+      <RenderChoice />
+    </div>
+  );
+}
 
 export default App;
